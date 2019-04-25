bfe623207b2eb56e8cec03867a21685a5b1eae0a
jdoleo <deoleo14@hotmail.com>
2019-04-25 07:27:52 -0400

    Input, output y viewchild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,15 @@ import { APP_ROUTES } from './app.routes';
 import { PagesModule } from './pages/pages.module';
 // import { AppRoutingModule } from './app-routing.module';
 
+// Temporal
+import { FormsModule } from '@angular/forms';
+
 // Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+
+
 
 
 
@@ -27,7 +32,8 @@ import { RegisterComponent } from './login/register.component';
     BrowserModule,
     // AppRoutingModule,
     APP_ROUTES,
-    PagesModule
+    PagesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 
+import { PAGES_ROUTES } from './pages.routes';
+
 import { SharedModule } from '../shared/shared.module';
+
+import { FormsModule } from '@angular/forms';
+
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
-import { PAGES_ROUTES } from './pages.routes';
+
+// Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
+
 
 
 @NgModule({
@@ -15,6 +27,8 @@ import { PAGES_ROUTES } from './pages.routes';
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -23,7 +37,9 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 export class PagesModule { }
